f85038c4c2028c4ea1c659bae24fc5dbdf7e3247
dmitry <joydnby@gmail.com>
2020-04-26 19:03:08 +0300

    fix: linter errors in auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,22 +2,25 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET_KEY } = require('../common/config');
 
 const authMiddleware = (req, res, next) => {
+  if (req.originalURL === 'login') {
+    return next();
+  }
   if (req.headers.authorization) {
     const [prefix, token] = req.headers.authorization.split(' ');
     if (prefix === 'Bearer') {
       jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
-          res.status(403).send({
+          res.status(401).send({
             success: false,
             error: 'failed to verify token'
           });
         }
         req.decoded = decoded;
-        next();
+        return next();
       });
     }
   } else {
-    res.status(403).send({
+    res.status(401).send({
       success: false,
       message: 'No token provided'
     });
