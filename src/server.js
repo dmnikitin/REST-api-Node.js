1a784e2e230ce031958fe0b4f2ace16c9e4fdfd1
dmitry <joydnby@gmail.com>
2020-04-26 19:01:25 +0300

    feat: add mongoose connection handler

--- src/server.js
+++ src/server.js
@@ -1,21 +1,33 @@
 const mongoose = require('mongoose');
 const app = require('./app');
-const logger = require('./common/winston-config');
 const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
+const logger = require('./common/winston-config');
+const User = require('./resources/users/user.model');
 
-mongoose.connect(MONGO_CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const connectToMongo = () => {
+  mongoose.connect(MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
-const db = mongoose.connection.once('open', () => {
-  console.log('mongodb connected');
-  db.dropDatabase();
+  const db = mongoose.connection;
 
-  app.listen(PORT, () =>
-    console.log(`App is running on http://localhost:${PORT}`)
-  );
-});
+  db.once('open', () => {
+    console.log('mongoDB connection success');
+    db.collection('users').drop();
+    app.listen(PORT, () =>
+      console.log(`App is running on http://localhost:${PORT}`)
+    );
+    const admin = new User({
+      name: 'admin',
+      login: 'admin',
+      password: 'admin'
+    });
+    admin.save();
+  });
+};
+
+connectToMongo();
 
 process.on('uncaughtException', err => {
   logger.error({ statusCode: 500, message: err.message });
